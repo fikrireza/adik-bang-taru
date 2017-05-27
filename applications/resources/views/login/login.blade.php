d9b8186293eaf09fb61d4d6b8e7aa74928aabd2d
dudyali <dudyali@gmail.com>
2017-05-27 10:23:01 +0700

    login mock-up

--- applications/resources/views/login/login.blade.php
+++ applications/resources/views/login/login.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-    
 <head>
-        <title>Matrix Admin</title><meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="css/bootstrap.min.css" />
-		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
-        <link rel="stylesheet" href="css/matrix-login.css" />
-        <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-
-    </head>
+  <title>Matrix Admin</title><meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="{{asset('theme/css/bootstrap.min.css')}}" />
+	<link rel="stylesheet" href="{{asset('theme/css/bootstrap-responsive.min.css')}}" />
+  <link rel="stylesheet" href="{{asset('theme/css/matrix-login.css')}}" />
+  <link href="{{asset('theme/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+</head>
     <body>
-        <div id="loginbox">            
+        <div id="loginbox">
             <form id="loginform" class="form-vertical" action="index.html">
-				 <div class="control-group normal_text"> <h3><img src="img/logo.png" alt="Logo" /></h3></div>
+			          <div class="control-group normal_text">
+                  {{-- <h3><img src="img/logo.png" alt="Logo" />ADIK</h3> --}}
+                  <h3 style="margin-bottom:0px;">ADIK BANG TARU</h3>
+                  Aplikasi Data dan Informasi Keuangan dan Bangunan
+                  <br><span style="font-size:10px;">Dinas Tata Ruang dan Bangunan, Kabupaten Tangerang.</span>
+                </div>
                 <div class="control-group">
                     <div class="controls">
                         <div class="main_input_box">
@@ -30,28 +33,12 @@
                     </div>
                 </div>
                 <div class="form-actions">
-                    <span class="pull-left"><a href="#" class="flip-link btn btn-info" id="to-recover">Lost password?</a></span>
-                    <span class="pull-right"><a type="submit" href="index.html" class="btn btn-success" /> Login</a></span>
-                </div>
-            </form>
-            <form id="recoverform" action="#" class="form-vertical">
-				<p class="normal_text">Enter your e-mail address below and we will send you instructions how to recover a password.</p>
-				
-                    <div class="controls">
-                        <div class="main_input_box">
-                            <span class="add-on bg_lo"><i class="icon-envelope"></i></span><input type="text" placeholder="E-mail address" />
-                        </div>
-                    </div>
-               
-                <div class="form-actions">
-                    <span class="pull-left"><a href="#" class="flip-link btn btn-success" id="to-login">&laquo; Back to login</a></span>
-                    <span class="pull-right"><a class="btn btn-info"/>Reecover</a></span>
+                  <center>
+                    <input type="reset" class="btn btn-danger" value="Reset">
+                    <input type="submit" class="btn btn-success" value="Login">
+                  </center>
                 </div>
             </form>
         </div>
-        
-        <script src="js/jquery.min.js"></script>  
-        <script src="js/matrix.login.js"></script> 
     </body>
-
 </html>
